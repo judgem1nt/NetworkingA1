21c987442c8392183a8932339d67299f618415bf
SimulatedGrowth <guvencdeniz.karaslan@hotmail.com>
2024-10-22 20:17:03 -0400

    Retrieve and Display Scoreboard

--- main.cpp
+++ main.cpp
@@ -67,7 +67,6 @@ int main(int argc, char* argv[]) {
         return -1;
     }
     TCPsocket client = init("127.0.0.1", 8080);
-    
 
     SDL_Window* window = SDL_CreateWindow("Space Invaders", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
--- networking.cpp
+++ networking.cpp
@@ -28,22 +28,15 @@ void cleanup(TCPsocket& server)
 
 void networkLoop(TCPsocket& client, string username_, int totalscore_)
 {
-    //username = username_;
-    //totalscore = totalscore_;
-    //char buffer[1024];
-    char buffer[1024];
-
+    char buffer[1024];  // Buffer for sending/receiving data
     bool clientLoop = true;
 
     while (clientLoop)
     {
-        printf("Enter a message to send (or type 'quit' to exit): ");
+        printf("Enter a message to send (or type 'quit' to exit, 'print' to get the scoreboard): ");
         fflush(stdout);
 
-        //SEND Data
-        //####################################################################
-        //####################################################################
-            // Read user input
+        // Read user input
         if (fgets(buffer, sizeof(buffer), stdin) == NULL) {
             break;  // Exit the loop on EOF or error
         }
@@ -54,36 +47,51 @@ void networkLoop(TCPsocket& client, string username_, int totalscore_)
             buffer[len - 1] = '\0';
         }
 
-        std::string new_message = username_ + " " + to_string(totalscore_);
+        std::string userInput = buffer;
 
-        //SDLNet_TCP_Send(client, buffer, sizeof(buffer));
-        if (client)
-            SDLNet_TCP_Send(client, new_message.c_str(), new_message.length() + 1);
-
-        if (strcmp(buffer, "quit") == 0) {
-            break;  // Exit the loop if the user enters 'quit'
+        // If the user types 'quit', exit the loop
+        if (userInput == "quit") {
+            break;
         }
-        //END SEND Data
-        //####################################################################
-        //####################################################################
 
-        //Recieve DATA
-        //####################################################################
-        //####################################################################
+        // If the user types 'print', request the scoreboard
+        if (userInput == "print") {
+            // Send "print" command to the server
+            std::string printCommand = "print";
+            if (client) {
+                SDLNet_TCP_Send(client, printCommand.c_str(), printCommand.length() + 1);
+            }
+
+            // Wait for the server to send the scoreboard
+            int received = SDLNet_TCP_Recv(client, buffer, sizeof(buffer));
+            if (received > 0) {
+                buffer[received - 1] = '\0';  // Null-terminate the received data
+                printf("SCOREBOARD:\n%s\n", buffer);  // Print the received scoreboard
+            }
+            else {
+                printf("Failed to receive scoreboard from the server.\n");
+            }
+
+            continue;  // Skip sending user input to the server for this loop iteration
+        }
+
+        // Otherwise, send the user's message (along with username and score) to the server
+        std::string new_message = username_ + " " + to_string(totalscore_) + ": " + userInput;
+        if (client) {
+            SDLNet_TCP_Send(client, new_message.c_str(), new_message.length() + 1);
+        }
+
+        // Wait for a response from the server
         int received = SDLNet_TCP_Recv(client, buffer, sizeof(buffer));
         if (received > 0) {
-
             buffer[received - 1] = '\0';  // Null-terminate the received data
-            if (strcmp(buffer, "exit") == 0)
-            {
-                printf("SERVER WAnts to close\n");
-                break;  // Exit the loop if the user enters 'quit'
-
+            if (strcmp(buffer, "exit") == 0) {
+                printf("SERVER wants to close the connection.\n");
+                break;  // Exit the loop if the server sends "exit"
             }
-            else
-                printf("SERVER: %s\n", buffer);
+            else {
+                printf("SERVER: %s\n", buffer);  // Print the server's response
+            }
         }
-        //####################################################################
-        //####################################################################
     }
 }
